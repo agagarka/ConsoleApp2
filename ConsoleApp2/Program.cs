c6ddc18a84eb609d4a39fc85c77cda8acb3d41ab
Логинова Юлия Сергеевна <loginova.ys1510@okeit.edu>
2022-10-08 11:14:49 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 6;
+            while (i > 7)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+
+            do
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            while (i > 7);
+        }
+    }
+}
